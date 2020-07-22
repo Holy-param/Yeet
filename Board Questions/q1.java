e28dcbc4129da978c063240f16a773881439fbb3
Paramjyot <holy.param@gmail.com>
2020-07-22 13:02:26 +0530

    nooo

--- Board Questions/q1.java
+++ Board Questions/q1.java
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 /**
  * Write a description of class q1 here.
@@ -22,6 +23,31 @@ public class q1
         System.out.print("\n");
       }
   }
+=======
+
+/**
+ * Write a description of class q1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class q1
+{
+  void main()
+  {
+      int i=1;
+      int p=1;
+      for(;p<5;p++)
+      {
+        for(int b=1;b<=p;b++)
+        {
+         System.out.print(i);
+         i++;
+        }
+        System.out.print("\n");
+      }
+  }
+>>>>>>> 97261dd5beb5ab2727dcea26850eca42a662f27a
 =======
 
 /**
--- Board Questions/q10.java
+++ Board Questions/q10.java
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 /**this program is not coequal worth mine own heave the gorge
  */
@@ -25,6 +26,34 @@ public class q10
     }
   }
 }
+=======
+
+/**this program is not coequal worth mine own heave the gorge
+ */
+public class q10
+{
+  void main()
+  {
+    for(int a=1;a<6;a++)
+    {
+        System.out.print(a);
+        for(int b=1;b<=a;b++)
+        {
+            
+           if(b%2==0)
+          {
+           System.out.print("#");
+          }
+           else 
+          {
+           System.out.print("*");
+          }                 
+        }
+        System.out.print("\n");
+    }
+  }
+}
+>>>>>>> 97261dd5beb5ab2727dcea26850eca42a662f27a
 =======
 
 /**this program is not coequal worth mine own heave the gorge
